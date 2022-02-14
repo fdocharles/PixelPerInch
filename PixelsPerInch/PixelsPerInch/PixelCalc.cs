3f65a903a67cd6fb0d2116f5c029944da9600eee
fdocharles <fdocharles@gmail.com>
2022-02-14 12:13:56 -0500

    Added pixle per inch console application

---
+++ PixelsPerInch/PixelsPerInch/PixelCalc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelsPerInch
+{
+    public static class PixelCalc
+    {
+        public static void Calculate(int width, int height, double diagonalSize)
+        {
+            double diagonalInPixels, ppi, dotPitch;
+            
+            diagonalInPixels = Math.Round(Math.Sqrt((width * width) + (height * height)), 2);
+
+            ppi = Math.Round(diagonalInPixels / diagonalSize,2);
+
+            dotPitch = Math.Round(25.4 / (Math.Sqrt((width * width) + (height * height)) / diagonalSize), 4);
+
+            Console.WriteLine($"PPI = {ppi}");
+            Console.WriteLine($"Diagonal = {diagonalInPixels} pixels");
+            Console.WriteLine($"Dot Pitch = {dotPitch} mm");
+
+        }
+    }
+}
---
+++ PixelsPerInch/PixelsPerInch/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace PixelsPerInch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Start();
+        }
+
+        public void Start()
+        {
+            bool isInValidOption = true;
+            string optionTxt = string.Empty;
+            int option;
+            bool isExit = false;
+            int width, height;
+            double diagonal;
+            string widthTxt, diagonalTxt, heightTxt;
+            bool isInValidWidth = true, isInValidDiagonal = true, isInValidHeight = true;
+
+            do
+            {
+                Console.WriteLine("******* Pixels per Inch Calculator ********");
+                Console.WriteLine();
+                Console.WriteLine("1. Enter Screen dimensions");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine("");
+
+                do {
+                    Console.Write("Please enter a valid option from the menu : ");
+
+                    optionTxt = Console.ReadLine();
+
+                    if (Int32.TryParse(optionTxt, out option) && ( option == 1 || option == 2 ))
+                    {
+                        isInValidOption = false;
+
+                    }
+                    else
+                    {
+                        isInValidOption = true;
+                    }
+
+                } while (isInValidOption);
+
+                if (option == 1)
+                {
+                    Console.WriteLine();
+
+                    //Get width
+                    do
+                    {
+                        Console.Write("Please enter width : ");
+                        widthTxt = Console.ReadLine();
+                        if (Int32.TryParse(widthTxt, out width))
+                        {
+                            isInValidWidth = false;
+
+                        }
+                        else
+                        {
+                            isInValidWidth = true;
+                            Console.WriteLine("Invalid width.");
+                            Console.WriteLine();
+                        }
+                    } while (isInValidWidth);
+
+                    //Get Height
+                    do
+                    {
+                        Console.Write("Please enter height : ");
+                        heightTxt = Console.ReadLine();
+                        if (Int32.TryParse(heightTxt, out height))
+                        {
+                            isInValidHeight = false;
+
+                        }
+                        else
+                        {
+                            isInValidHeight = true;
+                            Console.WriteLine("Invalid height.");
+                            Console.WriteLine();
+                        }
+                    } while (isInValidHeight);
+
+
+                    //Get Diagonal
+                    do
+                    {
+                        Console.Write("Please enter Diagonal (in pixels) : ");
+                        diagonalTxt = Console.ReadLine();
+                        if (Double.TryParse(diagonalTxt, out diagonal))
+                        {
+                            isInValidDiagonal = false;
+
+                        }
+                        else
+                        {
+                            isInValidDiagonal = true;
+                            Console.WriteLine("Invalid Diagonal.");
+                            Console.WriteLine();
+                        }
+                    } while (isInValidDiagonal);
+
+
+                    Console.WriteLine();
+                    Console.WriteLine("-- Results --");
+                    Console.WriteLine();
+                    PixelCalc.Calculate(width, height, diagonal);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+                else if (option == 2)
+                {
+                    isExit = true;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid input.");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            } while (!isExit);
+        }
+    }
+}
